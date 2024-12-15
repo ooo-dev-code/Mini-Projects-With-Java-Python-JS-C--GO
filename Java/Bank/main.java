07b05fb8945fa6f3c2d94dc1ab867483311da715
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2024-12-15 17:04:02 +0100

    Update main.java

--- Java/Bank/main.java
+++ Java/Bank/main.java
@@ -5,59 +5,56 @@ public class main {
     public static void main(String[] args) {
 
         Scanner bank = new Scanner(System.in);
-        Bank account = new Bank();
+        int pin = 1894;
+        int savings = 1000;
 
-        System.out.print("Welcome to the bank ");
+        System.out.println("Welcome to the bank ");
         System.out.println("1 - Enter Account");
         System.out.println("2 - Create Account");
 
         int user_response_0 = bank.nextInt();
         if (user_response_0 == 1) {
-            enter_bank(user_response_0, bank, account);
+            enter_bank(user_response_0, bank, pin, savings);
         } else if (user_response_0 == 2) {
             
         }
 
     }
 
-    static void enter_bank(int user_response_0, Scanner bank, Bank account) {
+    static void enter_bank(int user_response_0, Scanner bank, int pin, int savings) {
         System.out.print("Enter your code pin: ");
         int pin_response = bank.nextInt();
 
-        if (account.pin == pin_response) {
+        if (pin == pin_response) {
             
             System.out.println("1 - Check Account");
             System.out.println("2 - Empty Account");
             System.out.println("Others - Exit");
             int user_response = bank.nextInt();
 
-            switch (user_response) {
-                case 1:
-                    System.out.println("Account: " + account.savings);
+            if (user_response==1) {
+                    System.out.println("Account: " + savings);
                     System.out.println("1 - Add to Account");
                     System.out.println("2 - Take to Account");
                     System.out.println("Others - Exit");
                     int user_response_2 = bank.nextInt();
-                    switch (user_response_2) {
-                        case 1:
+                    if (user_response_2==1) {
                             System.out.println("How much ?");
                             int value = bank.nextInt();
-                            account.savings = account.savings + value;
-                            System.out.println("Account: " + account.savings);
-                            break;
-                    
-                        case 2:
-                            System.out.println("How much ?");
-                            int value = bank.nextInt();
-                            account.savings = account.savings + value;
-                            System.out.println("Account: " + account.savings);
-                            break;
+                            savings = savings + value;
+                            System.out.println("Account: " + savings);
                     }
-                
-                case 2:
-                    account.savings = 0;
-                    System.out.println("Account: " + account.savings);
-                    break;
+                    
+                    if (user_response_2==2) {
+                        System.out.println("How much ?");
+                        int values = bank.nextInt();
+                        savings = savings - values;
+                        System.out.println("Account: " + savings);
+                    }
+            }
+            if (user_response==2) {
+                savings = savings*0;
+                System.out.println("Account: " + savings);
             }
 
         } else {
