0e7d9259a15aa4a2977d6e9adb1b3d9356727acd
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2024-12-16 19:29:02 +0100

    Update main.java

--- Java/Dice_Surprise_Game/main.java
+++ Java/Dice_Surprise_Game/main.java
@@ -1,1 +1,27 @@
 
+import java.util.HashMap;
+import java.util.Random;
+
+public class dice {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> names = new HashMap<Integer, String>();
+        names.put(1, "James");
+        names.put(2, "John");
+        names.put(3, "Jeremy");
+        names.put(4, "Jeff");
+        names.put(5, "Johnson");
+        names.put(6, "Jenny");
+
+        Random random = new Random();
+
+        int num = random.nextInt(6)+1;
+        System.out.println("Name: " + names.get(num));
+
+        int num2 = random.nextInt(99)+1;
+        System.out.println("Age: " + num2);
+
+    }
+    
+}
