b38e8925f771dc796a5f6a80f99a73d6c42c0670
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2025-01-08 12:11:14 +0100

    Update WordApp.java

--- Java/Word/WordApp.java
+++ Java/Word/WordApp.java
@@ -1,87 +1,72 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
-public class WordApp extends JFrame implements ActionListener {
-    JTextArea textArea;
-    JScrollPane scrollPane;
-    JMenuBar menuBar;
-    JMenu fileMenu;
-    JMenuItem newMenuItem, openMenuItem, saveMenuItem, exitMenuItem;
+public class Foo{
 
-    public WordApp() {
-        setTitle("Word App");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  public static void main(String[] args) {
 
-        textArea = new JTextArea();
-        scrollPane = new JScrollPane(textArea);
-        add(scrollPane);
+    JFrame f = new JFrame("A JFrame");
+    f.setSize(500, 500);
+    f.setLocation(300,200);
 
-        menuBar = new JMenuBar();
-        fileMenu = new JMenu("File");
-        menuBar.add(fileMenu);
+    final JTextArea textArea = new JTextArea(1, 1);
+    textArea.setLineWrap(true);
+    textArea.setWrapStyleWord(true);
+    f.getContentPane().add(BorderLayout.CENTER, textArea);
+    JButton delete_b = new JButton("Delete");
+    f.getContentPane().add(BorderLayout.NORTH, delete_b);
 
-        newMenuItem = new JMenuItem("New");
-        openMenuItem = new JMenuItem("Open");
-        saveMenuItem = new JMenuItem("Save");
-        exitMenuItem = new JMenuItem("Exit");
+    JButton save_b = new JButton("Save");
+    f.getContentPane().add(BorderLayout.WEST, save_b);
 
-        newMenuItem.addActionListener(this);
-        openMenuItem.addActionListener(this);
-        saveMenuItem.addActionListener(this);
-        exitMenuItem.addActionListener(this);
+    save_b.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JFrame f = new JFrame("A JFrame");
+        f.setSize(500, 100);
+        f.setLocation(300,200);
 
-        fileMenu.add(newMenuItem);
-        fileMenu.add(openMenuItem);
-        fileMenu.add(saveMenuItem);
-        fileMenu.add(exitMenuItem);
+        JLabel title = new JLabel("Name of your file in txt");
+        f.getContentPane().add(BorderLayout.NORTH, title);
+        final JTextArea saveArea = new JTextArea(1, 1);
+        saveArea.setBackground(java.awt.Color.CYAN);
+        f.getContentPane().add(BorderLayout.CENTER, saveArea);
+        JButton save_b2 = new JButton("Save");
+        f.getContentPane().add(BorderLayout.SOUTH, save_b2);
 
-        setJMenuBar(menuBar);
-    }
+        save_b2.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == newMenuItem) {
-            textArea.setText("");
-        } else if (e.getSource() == openMenuItem) {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
-            int option = fileChooser.showOpenDialog(this);
-            if (option == JFileChooser.APPROVE_OPTION) {
-                try {
-                    BufferedReader reader = new BufferedReader(new FileReader(fileChooser.getSelectedFile()));
-                    textArea.read(reader, null);
-                    reader.close();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+            String file_name = saveArea.getText();
+            file_name = file_name.replace(" ", "_");
+            try (java.io.FileWriter writer = new java.io.FileWriter(file_name + ".txt")) {
+              System.out.println(file_name);
+              writer.write(textArea.getText());
+            } catch (java.io.IOException ex) {
+              ex.printStackTrace();
             }
-        } else if (e.getSource() == saveMenuItem) {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
-            int option = fileChooser.showSaveDialog(this);
-            if (option == JFileChooser.APPROVE_OPTION) {
-                try {
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(fileChooser.getSelectedFile()));
-                    textArea.write(writer);
-                    writer.close();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        } else if (e.getSource() == exitMenuItem) {
-            System.exit(0);
-        }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            WordApp app = new WordApp();
-            app.setVisible(true);
+          }
         });
-    }
+
+        f.setVisible(true);
+      }
+    });
+
+    delete_b.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            textArea.setText("");
+        }
+    });
+
+    f.setVisible(true);
+
+  }
+
 }
